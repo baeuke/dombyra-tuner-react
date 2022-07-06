40b58fcd798ea0213d8a6ddc442da4ed1003171c
Baurzhan <baurzhan@Baurzhans-MacBook-Pro.local>
2022-07-06 16:36:50 +0600

    audio func async added

--- src/components/tuner.jsx
+++ src/components/tuner.jsx
@@ -7,6 +7,8 @@ import ml5 from "ml5";
 // ! если разница между настоящим звуком и следующим слишком большая, то не изменять положение пойнтера
 
 
+const audio = new Audio("tuner.mp3");
+
 let audioContext;
 let pitch;
 
@@ -36,16 +38,16 @@ function modelLoaded() {
    console.log("model's loaded");
 }
 
-// const playAudio = async () => {
-//       const importRes = await import("tuning_Audio.mp3"); // make sure the path is correct
-//       var audio = new Audio(importRes.default);
-//       try {
-//          await audio.play();
-//          console.log("Playing audio");
-//       } catch (err) {
-//          console.log("Failed to play, error: " + err);
-//       }
-// }
+const playAudio = async () => {
+   // const importRes = await import("./audio/tuner.mp3"); // make sure the path is correct
+   // var audio = new Audio(importRes.default);
+   try {
+      await audio.play();
+      console.log("Playing audio");
+   } catch (err) {
+      console.log("Failed to play, error: " + err);
+   }
+}
 // let dFunc = () => {
 //       diff = freq - 146.8324;
 //       absDiff = Math.abs(diff);
@@ -72,44 +74,80 @@ function modelLoaded() {
 
 export const Tuner = () => {
    const [frequency, setFrequency] = useState({
-      diffD:0,
-      diffG:0,
-      freq:0,
+      diffD: 0,
+      diffG: 0,
+      freq: 0,
    });
    const [note, setNote] = useState("");
    // const [audio, setAudio] = useState(false);
 
-   const audio = new Audio("tuner.mp3");
-   
+
    // const [position, setPosition] = useState('calc(50% - 3px)');
 
 
    // console.log(pitch);
 
+   // const getFreq = () => {
+   //    console.log('pitch', pitch);
+   //    if (pitch) {
+   //       pitch.getPitch((err, frq) => {
+   //          if (frq) {
+   //             // freq = frequency
+
+
+   //             // diffG = frq - 195.9977;
+
+   //             // diffD = frq - 146.8324;
+   //             // diffG = freq - 195.9977;
+   //             // helper (freq);
+   //             setFrequency({
+   //                diffD: frq - 146.8324,
+   //                diffG: frq - 195.9977,
+   //                freq: frq.toFixed(2)
+   //             });
+   //             // help ();
+
+   //             // setFrequency(freq);
+
+   //          } else {
+   //             setFrequency((prev) => ({ ...prev, freq: "No pitch detected" }));
+   //          }
+   //          setTimeout(() => {
+   //             getFreq()
+   //          }, 60);
+   //       });
+   //    }
+   // }
+
    useEffect(() => {
+      // console.log('1');
+      // getFreq()
+      // console.log('2');
       const interval = setInterval(() => {
          if (pitch) {
             pitch.getPitch((err, frq) => {
                if (frq) {
                   // freq = frequency
 
-                  
+
                   // diffG = frq - 195.9977;
-                  
+
                   // diffD = frq - 146.8324;
                   // diffG = freq - 195.9977;
                   // helper (freq);
                   setFrequency({
-                     diffD:frq - 146.8324,
-                     diffG:frq - 195.9977,
-                     freq:frq.toFixed(2)});
-                  // help ();
-                  
+                     diffD: frq - 146.8324,
+                     diffG: frq - 195.9977,
+                     freq: frq.toFixed(2)
+                  });
+
                   // setFrequency(freq);
                } else {
                   setFrequency((prev)=>({...prev,freq:"No pitch detected"}));
                }
+
             });
+
          }
       }, 50);
       return () => {clearInterval(interval)};
@@ -137,7 +175,7 @@ export const Tuner = () => {
    //    }
    //    console.log(diff);
    // };
-   
+
 
 
    let dColorClass = (note === "D") ? " green" : "";
@@ -145,7 +183,7 @@ export const Tuner = () => {
 
    // let ptrPos = (note == "D") ? dFunc() : 
    //    (note == "G") ? gFunc() : "calc(50% - 3px)";
-   
+
    // if (note == "D") {
    //    absDiff = Math.abs(diffD);
    //    if (absDiff < 1) {
@@ -167,7 +205,7 @@ export const Tuner = () => {
    //       ptrPos = `calc(50% - 3px + ${absDiff}px)`;
    //    }
    // }
-   
+
 
    //RABOCHIY: *******
    // let pStyle = {
@@ -180,13 +218,13 @@ export const Tuner = () => {
       left: `30%`
    };
 
-   if (note == "G" && frequency.freq && frequency.diffG) {
+   if (note == "G" && frequency.freq) {
       const absDiff = Math.abs(frequency.diffG);
       if (absDiff < 1) {
          pStyle.left = 'calc(50% - 3px)';
-         // playAudio();
-         
-         audio.play();
+         playAudio();
+
+         // audio.play();
          console.log("Audio had to play");
       } else if (frequency.diffG < -2) {
          pStyle.left = `calc(50% - 3px - ${absDiff}px)`;
@@ -195,20 +233,20 @@ export const Tuner = () => {
       }
    }
 
-   if (note == "D"  && frequency.freq && frequency.diffD) {
+   if (note == "D" && frequency.freq && frequency.diffD) {
       const absDiff = Math.abs(frequency.diffD);
       if (absDiff <= 1) {
          pStyle.left = 'calc(50% - 3px)';
-         // playAudio();
-         audio.play();
-      } else if (frequency.diffD < (-1*threshold)) {
+         playAudio();
+         // audio.play();
+      } else if (frequency.diffD < (-1 * threshold)) {
          pStyle.left = `calc(50% - 3px - ${absDiff}px)`;
       } else if (frequency.diffD > threshold) {
          pStyle.left = `calc(50% - 3px + ${absDiff}px)`;
       }
    }
    // helper () {
-      
+
    // }
    return (
       <>
@@ -227,14 +265,14 @@ export const Tuner = () => {
                   setNote("D");
                }}
             >D</button>
-            
+
             <button
                className={`g-note${gColorClass}`}
                onClick={() => {
                   setNote("G");
                }}
             >G</button>
-            <div className="img"><img width="390px" src={"paintDombyra.png"}/></div>
+            <div className="img"><img width="390px" src={"paintDombyra.png"} /></div>
             <div className="num">{frequency.freq}</div>
          </div>
       </>
